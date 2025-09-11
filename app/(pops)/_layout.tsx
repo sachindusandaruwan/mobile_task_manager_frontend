c402b46f6d6ac62c6824c2418b2098a6b14bcad8
sachindusandaruwan <sadaruwansachindu@gmail.com>
2025-09-11 14:10:15 +0530

    clear the code

--- app/(pops)/_layout.tsx
+++ app/(pops)/_layout.tsx
@@ -1,28 +1,3 @@
-//gpt denna kalin eka
-
-// import { Stack } from "expo-router";
-//
-// export default function PopLayout() {
-//     return (
-//         <Stack screenOptions={{headerShown: false}}>
-//             <Stack.Screen name="addPop"/>
-//             <Stack.Screen name="updateDeletePop"/>
-//         </Stack>
-//     );
-// }
-
-
-// import { Stack } from "expo-router";
-//
-// export default function PopsLayout() {
-//     return (
-//         <Stack screenOptions={{ headerShown: false }}>
-//             <Stack.Screen name="addPop" />
-//             <Stack.Screen name="updateDeletePop" />
-//         </Stack>
-//     );
-// }
-
 
 import { Stack } from "expo-router";
 
