22a019e4a1d3c4038b7c25ecd953692b612c79e6
sachindusandaruwan <sadaruwansachindu@gmail.com>
2025-09-23 10:54:41 +0530

    fix errors

--- api/setupApi.ts
+++ api/setupApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base axios instance (no redux, no interceptors)
 const setupApi = axios.create({
-    baseURL: "http://10.41.112.8:3000",
+    baseURL: "http://172.20.154.8:3000",
     withCredentials: true,
     headers: { "Content-Type": "application/json" },
 
